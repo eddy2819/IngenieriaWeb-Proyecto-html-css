e3a57b7b322bcd643bb19941784bbad7303be26e
Eddy Guarnizo <73207797+eddy2819@users.noreply.github.com>
2022-11-08 17:06:28 -0500

    code modification

---
+++ mascotas/index.php
@@ -0,0 +1,72 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Happy Pets</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+	<link rel="icon" href="images/logo.png" sizes="32x32" />
+    <link rel="icon" href="images/logo.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="images/logo.png" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+</head>
+<body>
+<div class="contenedor">
+	<header class="cabeceraPrincipal">
+		<section class="logotipo"><img src="images/logotipoPet.png"></section>
+		<nav class="menuPrincipal">
+			<a href="#">Inicio</a>
+			<a href="Internas/servicios.php">Servicios</a>
+			<a href="#">Productos</a>
+			<a href="#">Adopción</a>
+			<a href="#">Contactos</a>
+		</nav>
+	</header>
+	<section class="sliderPrincipal">
+		<img src="images/slider.jpg">
+	</section>
+	<section class="titulo"><h3 class="colorAzul"> Adopcion</h3></section>
+	<section class="adopcion1">
+		<img src="images/dog1.png" title="Pastor" description= "perro en adopcion">
+		<article class="card1">
+			<h3 class="mayusculas colorAzul">Pastor</h3>
+			<p>It is a medium-sized dog, white in color with brown spots. His fur is soft as silk. It has short and strong legs. Its ears are small and its snout is slightly flattened. The spot on his chest is in the shape of a rhombus, this makes him very special. It wags its tail a thousand times when it greets you. He is very affectionate and I love him very much.
+			</p>
+			<div class="Derecha">
+				<button class="button button1">Mas Informacion</button>
+			</div>
+			
+		</article>
+	</section>
+	<section class="adopcion2">
+		<img src="images/dog2.png" title="Robles">
+		<article class="card1">
+			<h3 class="mayusculas colorAzul">Robles</h3>
+			<p>It is a medium-sized dog, white in color with brown spots. His fur is soft as silk. It has short and strong legs. Its ears are small and its snout is slightly flattened. The spot on his chest is in the shape of a rhombus, this makes him very special. It wags its tail a thousand times when it greets you. He is very affectionate and I love him very much.
+			</p>
+			<div class="Derecha"> 
+				<button class="button button1">Mas Informacion</button>
+			</div>
+		</article>
+	</section>
+	<section class="doctor">
+		<h3>Doctores</h3>
+	</section>
+	<section class="sponsor">
+		<h3>Sponsor</h3>
+	</section>
+	<footer class="piePagina">
+		<section class="derechos">
+			<h6>Derechos Reservados UTPL 2022</h6>
+		</section>
+		<nav class="redesSociales">
+			<a href=""><i class="fa-brands fa-facebook"></i></a>
+			<a href=""><i class="fa-brands fa-instagram"></i></a>
+			<a href=""><i class="fa-brands fa-tiktok"></i></a>
+		</nav>
+	</footer>
+</div>
+</body>
+</html>
